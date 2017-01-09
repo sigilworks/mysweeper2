f52f694a766290109a38a74e884af685b1e434d1
sigilworks <tj@sigilworks.com>
2017-01-09 16:40:16 -0500

    porting over legacy danger calculator to ES6

---
+++ src/app/components/utils/danger-calculator.js
@@ -0,0 +1,42 @@
+const _ = require('lodash'),
+      freeze = require('deep-freeze');
+
+export default class DangerCalculator {
+    constructor(gameboard) {
+        this.board = gameboard;
+    }
+
+    get neighborhood() {
+        return freeze({
+            // distance in steps from this square:
+            //           vert. horz.
+            NORTH:      [  1,  0 ],
+            NORTHEAST:  [  1,  1 ],
+            EAST:       [  0,  1 ],
+            SOUTHEAST:  [ -1,  1 ],
+            SOUTH:      [ -1,  0 ],
+            SOUTHWEST:  [ -1, -1 ],
+            WEST:       [  0, -1 ],
+            NORTHWEST:  [  1, -1 ]
+        });
+    }
+
+    forSquare(row, cell) {
+        if (row >= 0 && cell >= 0) {
+            const directions = _.keys(this.neighborhood),
+
+                  totalMines = _.reduce(directions, (total, direction) => {
+                    const vert = _.first(this.neighborhood[direction]),
+                          horiz = _.last(this.neighborhood[direction]),
+                          neighbor = this.board.getSquareAt(row + vert, cell + horiz);
+
+                    if (neighbor && neighbor.isMined()) 
+                        total++;
+
+                    return total;
+                }, 0);
+
+            return totalMines;
+        }
+    }
+}
